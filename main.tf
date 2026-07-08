1e1ffc86779405fbd02b174a23302881696614c8
PrashantUbale2019 <55487751+PrashantUbale2019@users.noreply.github.com>
2026-07-08 16:30:06 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "iac-terraform-rg" {
+  name     = "${local.prefix}-rg"
+  location = "westus2"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "iac-terraform-vnet" {
+  name                = "${local.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.iac-terraform-rg.name
+}
+
